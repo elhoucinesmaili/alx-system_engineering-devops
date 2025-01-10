99df0f5839184be11edd71c8fd91c9e42b91bf12
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-01-10 07:39:22 -0800

    git

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# This manifest creates a file in /tmp with specific ownership, permissions, and content.
 file { '/tmp/school':
   ensure  => 'file',
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
-  content => 'I love Puppet',
 }
