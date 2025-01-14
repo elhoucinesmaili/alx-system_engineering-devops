e04f399eb0d0a0a5937a88ff6d9f625a72de11d7
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-01-14 08:54:41 -0800

    git

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,38 +1,24 @@
-# Ensure system package index is up-to-date
-exec { 'update_system':
-  command => '/usr/bin/apt-get update',
+# this Ubuntu server with nginx
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
-# Install Nginx package
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['update_system'],
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-# Create a simple HTML file
-file { '/var/www/html/index.html':
-  content => "<html>
-  <head><title>Welcome</title></head>
-  <body>
-    <h1>Hello, Ubuntu Server!</h1>
-  </body>
-</html>",
-  require => Package['nginx'],
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-# Add redirect configuration in Nginx
-file_line { 'add_redirect_to_nginx':
-  path  => '/etc/nginx/sites-available/default',
-  line  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;',
-  match => 'location / {',
-  after => 'location / {',
-  require => Package['nginx'],
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-# Ensure Nginx service is running and apply changes
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  subscribe  => File['/etc/nginx/sites-available/default'],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
-
