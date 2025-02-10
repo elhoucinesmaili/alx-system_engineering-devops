3584dde560fd313a0c75bbfdade22c5e70c7691c
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-02-10 07:51:42 -0800

    git

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Export api to csv"""
+""" Script to export user tasks to a CSV file """
 import csv
 import requests
 import sys
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     user = sys.argv[1]
     url_user = 'https://jsonplaceholder.typicode.com/users/' + user
     res = requests.get(url_user)
-    """ANYTHING"""
+    """ Fetch user data """
     user_name = res.json().get('username')
     task = url_user + '/todos'
     res = requests.get(task)
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     with open('{}.csv'.format(user), 'w') as csvfile:
         for task in tasks:
             completed = task.get('completed')
-            """Complete"""
+            """ Task completion status """
             title_task = task.get('title')
-            """Done"""
+            """ Task title """
             csvfile.write('"{}","{}","{}","{}"\n'.format(
                 user, user_name, completed, title_task))
