2df8a08b2ea360d6fb3b2c7432a6be74076872ba
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-02-26 07:49:28 -0800

    git

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,6 @@
-# A Puppet script to replace occurrences of "phpp" with "php" in a file on the server
+# A puppet script to fix php settings file by replacing typos
 
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-# Replace any instance of "phpp" with "php" in the specified file
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+exec { 'fix_typo':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => ['/bin','/usr/bin']
 }
